dfddaf982b5da819a95c107f41cea47f611d2a83
Abel José <abeljose018@gmail.com>
2025-02-02 17:21:55 -0300

    Código funcional

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Temporizador_Periodico
+Tarefa do EmbarcaTech destinada a implementar um semáforo com delay de 3 segundos com a utilização da função add_repeating_timer_ms();
+
+# Intruções
+Para executar o código em questão, deve-se possuir a extensão Wokwi Simulator instalada no Visual Studio Code. 
+
+Acesse o arquivo diagram.json, com a extensão Wokwi Simulator instalada, para gerar o ambiente de simulação de uma placa RaspBerry Pi Pico W composto de 3 leds (vermelho, verde e amarelo), ligados a 3 resitores com 330Ω.
+
+Ao iniciado, começará imediatamente um loop de um semáforo, iniciando pela cor vermelha, após 3 segundos alternando a cor, primeiramente para amarelo e em seguida para verde. Por fim o loop é repetido até que se interrompa o programa.
+
+# Composição do código
+**Temporizador_Periodico.c**
+- `inicializar_pinos()`: Prepara os pinos dos botões e do LED para uso.
+- `luz_vermelha()`: Ativa o led vermelho. 
+- `luz_amarela()`: Ativa o led amarelo. 
+- `luz_verde()`: Ativa o led verde.
+- `repeating_timer_callback(struct repeating_timer *t)`: Função de callback do temporizador. 
+- `main`: Lógica que configura e inicia o programa.
+
+# Desenvolvedor
+Abel José Rocha Barros Bezerra
---
+++ Temporizador_Periodico.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/time.h"
+
+#define red_pin 11
+#define yellow_pin 12
+#define green_pin 13
+
+int state = 0;
+
+//  Função que inicializa os pinos 
+void incicializar_pinos(){
+    gpio_init(red_pin);
+    gpio_set_dir(red_pin, GPIO_OUT);
+
+    gpio_init(yellow_pin);
+    gpio_set_dir(yellow_pin, GPIO_OUT);
+
+    gpio_init(green_pin);
+    gpio_set_dir(green_pin, GPIO_OUT);
+}
+
+// Função que liga a luz vermelha 
+void luz_vermelha(){
+    gpio_put(red_pin, 1);
+    gpio_put(yellow_pin, 0);
+    gpio_put(green_pin, 0);
+}
+
+// Função que liga a luz amarela 
+void luz_amarela(){
+    gpio_put(red_pin, 0);
+    gpio_put(green_pin, 0);
+    gpio_put(yellow_pin, 1);
+}
+
+// Função que liga a luz verde
+void luz_verde(){
+    gpio_put(red_pin, 0);
+    gpio_put(green_pin, 1);
+    gpio_put(yellow_pin, 0);
+}
+
+// Função de callback do temporizador 
+bool repeating_timer_callback(struct repeating_timer *t){
+    switch(state){
+        case 0: 
+            luz_vermelha();
+            break;
+        case 1:
+            luz_amarela();
+            break;
+        case 2:
+            luz_verde();
+            break;
+    }
+    
+    state++; // Avança para para a próxima cor 
+
+    if(state > 2){
+        state = 0;
+    }
+    return true; // Retorna true para continuar o temporizador 
+}   
+
+int main()
+{
+    stdio_init_all();
+    incicializar_pinos();
+
+    // Declara a variável do temporizador
+    struct repeating_timer timer;
+
+    // Configura o temporizador para chamar a função de callback a cada 3 segundos
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
+    
+    // Chama o callback manualmente para iniciar imediatamente
+    repeating_timer_callback(NULL);
+
+    while (true) {
+        
+        printf("Tempo decorrido: 1 segundo\n");
+        sleep_ms(1000); // Espera 1 segundo
+        
+    }
+}
